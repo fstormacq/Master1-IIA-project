7bf8c4b44aac460dd480ba2bbc95de9e8cbc0984
Caribou26 <127028027+Loup-K@users.noreply.github.com>
2025-11-07 09:19:26 +0100

    add micro.py
    permet de situer de facon assez vague le niveau sonore que capte le micro

---
+++ micro/micro.py
@@ -0,0 +1,51 @@
+import sounddevice as sd
+import numpy as np
+import time
+
+frequence = 44100     
+bloc_duree = 0.1      
+device_name = "USB PnP Sound Device"  #This is the micro that we have to test but we can change it
+
+devices = sd.query_devices() 
+device_id = None
+for i, dev in enumerate(devices):
+    if dev['max_input_channels'] > 0 and device_name in dev['name']:
+        device_id = i
+        break
+
+if device_id is None:
+    raise RuntimeError(f"{device_name} can' be find.")
+
+print("We use: ", sd.query_devices(device_id)['name'])
+print("Now, we have to press Ctrl+C to stop the program.")
+
+def audio_callback(indata, frames, time_info, status): #main function
+    if status:
+        print(status)
+    rms = np.sqrt(np.mean(indata**2)) #calculate RMS
+    niveau_db = 20 * np.log10(rms + 1e-10)  
+    
+    if niveau_db < -45:
+        label = "Chillax"
+    elif niveau_db < -30:
+        label = "Some noise"
+    elif niveau_db < -15:
+        label = "Be Careful"
+    else:
+        label = "Danger"
+    
+    bar_length = int(np.clip((niveau_db + 60) / 2, 0, 30))
+    bar = "█" * bar_length
+    
+    print(f"\r{niveau_db:6.1f} dB {bar:<30} {label:<15}", end="")
+
+with sd.InputStream(device=device_id,
+                    channels=1,
+                    samplerate=frequence,
+                    blocksize=int(frequence * bloc_duree),
+                    callback=audio_callback):
+    try:
+        while True:
+            time.sleep(0.1)
+    except KeyboardInterrupt:
+        print("\n🛑 Stopped.")
