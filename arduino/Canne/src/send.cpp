bd809a838927bbc9dfc2adeb62ebad013642f4f9
FloSto-Bash <florian_stormacq@icloud.com>
2025-12-10 16:02:39 +0100

    try fix arduino with gemini 3

--- arduino/Canne/src/send.cpp
+++ arduino/Canne/src/send.cpp
@@ -4,36 +4,47 @@
 
 #define LED_PIN     9
 #define NUM_LEDS    3
-#define BRIGHTNESS  20
+#define BRIGHTNESS  75  // Augmenté à 75 pour être sûr que ce soit visible
 #define LED_TYPE    WS2812B
 #define COLOR_ORDER GRB
 CRGB leds[NUM_LEDS];
 
 void setupPins() {
-
+    // Initialise la LED interne (Pin 13 sur Uno) pour le debug visuel sur la carte
+    pinMode(LED_BUILTIN, OUTPUT);
+    
+    // Initialise FastLED
     FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
     FastLED.setBrightness(BRIGHTNESS);
 
-    // 🔴 TEST DE DÉMARRAGE : Allume les LEDs en ROUGE pendant 2 secondes
+    // 🔴 TEST DE DÉMARRAGE
     Serial.println("[LED] Test de démarrage - LEDs en ROUGE pendant 2s");
+    
+    // Allume la LED interne (témoin que le code passe ici)
+    digitalWrite(LED_BUILTIN, HIGH); 
+    
+    // Allume le ruban en ROUGE
     fill_solid(leds, NUM_LEDS, CRGB::Red);
     FastLED.show();
     delay(2000);
     
-    // Puis test en VERT pendant 1 seconde
+    // Test VERT
     Serial.println("[LED] Test VERT pendant 1s");
+    digitalWrite(LED_BUILTIN, LOW); // Eteint LED interne (clignotement)
     fill_solid(leds, NUM_LEDS, CRGB::Green);
     FastLED.show();
     delay(1000);
     
-    // Puis test en BLEU pendant 1 seconde
+    // Test BLEU
     Serial.println("[LED] Test BLEU pendant 1s");
+    digitalWrite(LED_BUILTIN, HIGH); // Rallume LED interne
     fill_solid(leds, NUM_LEDS, CRGB::Blue);
     FastLED.show();
     delay(1000);
 
-    // Éteint tout après le test
+    // Éteint tout
     stopAll();
+    digitalWrite(LED_BUILTIN, LOW);
     Serial.println("[LED] Tests terminés - LEDs éteintes");
 }
 
@@ -56,7 +67,7 @@ void stopAll() {
 }
 
 void applyIntensity(int leftVal, int centerVal, int rightVal) {
-    
+
     leds[0] = getColorFromIntensity(leftVal);   // Gauche
     leds[1] = getColorFromIntensity(centerVal); // Centre
     leds[2] = getColorFromIntensity(rightVal);  // Droite
